e886b9084163849ccdadc4543d7592467bc44324
Professorcleiton <159616293+Professorcleiton@users.noreply.github.com>
2026-08-12 13:45:53 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'siga-cache-v7'; // Incremente para v7, v8... a cada nova alteração no site
+const CACHE_NAME = 'siga-cache-v10'; // Incremente para v7, v8... a cada nova alteração no site
 
 const urlsToCache = [
     '/',
